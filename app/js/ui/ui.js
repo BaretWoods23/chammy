5d19aa5f11d5b264855b1bf680764746c0eccc5e
ealba2615 <ealba@student.neumont.edu>
2018-08-06 09:11:30 -0600

    Added sound and mute button to project

--- app/js/ui/ui.js
+++ app/js/ui/ui.js
@@ -49,6 +49,27 @@ var ui =
         return newButton;
     },
 
+    makeDefaultMuteButton: function(parent, text, x, y, callbackFunc)
+    {
+        var BUTTON_WIDTH = 50;
+        var BUTTON_HEIGHT = 50;
+
+        var newButton = new createjs.Container();
+        newButton.x = x - BUTTON_WIDTH / 2;
+        newButton.y = y;
+        parent.addChild(newButton);
+
+        var shape = new createjs.Shape();
+        shape.graphics.beginFill('#fff').drawRect(0, 0, BUTTON_WIDTH, BUTTON_HEIGHT);
+        newButton.addChild(shape);
+
+        var text = this.makeDefaultText(newButton, text, BUTTON_WIDTH / 2, BUTTON_HEIGHT / 2);
+
+        newButton.on("mousedown", callbackFunc);
+
+        return newButton;
+    },
+
     addDefaultBox: function(parent,x,y)
     {
         var BOX_WIDTH = 100;
